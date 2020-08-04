28989889f1f32951d583a4d749e345474ecd48bb
lylf <1014661165@qq.com>
2020-08-04 11:14:13 +0800

    新增g

---
+++ G.java
@@ -0,0 +1,19 @@
+package com;
+
+public class G{
+    public static void main(String[] args){
+        int g = 0;
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int mul(int a, int b){
+        return a*b;
+    }
+}
