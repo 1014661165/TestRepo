18a6e598171f025413d0755d7b6e39cfce5077c3
lylf <1014661165@qq.com>
2020-08-04 10:06:33 +0800

    修改a,b

--- B.java
+++ B.java
@@ -1,7 +1,23 @@
-package com
+package com;
 
 public class B{
     public static void main(String[] args){
         int b = 0;
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int mul(int a, int b){
+        return a*b;
+    }
+
+    public int div(int a, int b){
+        return a/b;
     }
 }
