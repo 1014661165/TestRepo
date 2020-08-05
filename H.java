a4a24be76d1e3ec14b3dd659e24cedd054214dbb
lylf <1014661165@qq.com>
2020-08-05 10:30:03 +0800

    新增H

---
+++ H.java
@@ -0,0 +1,19 @@
+package com;
+
+public class B{
+    public static void main(String[] args){
+        int b = 0;
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int div(int a, int b){
+        return a/b;
+    }
+}
